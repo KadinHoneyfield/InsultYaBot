6544b3cc9c7c86cfeeb9f3cae63ea656d9f8e02a
Kadin <kadinhoneyfield@gmail.com>
2022-03-04 22:23:21 +1300

    Add error handling

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const axios = require('axios');
+const axios = require("axios");
 const jsdom = require("jsdom");
 const { Client, Intents } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 const config = require("./config.json");
-const { JSDOM } = require('jsdom');
+const { JSDOM } = require("jsdom");
 
 client.on("ready", () => {
   console.log(`Logged in...`);
@@ -19,22 +19,29 @@ const prefix = `!`;
 client.on("message", async function (message) {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
+
   // get channel id and command out of message
   const channelId = message.channel.id;
   const commandBody = message.content.slice(prefix.length);
   const args = commandBody.split(" ");
   const command = args.shift().toLowerCase();
+  
   if (command === "insult") {
     // after successfully create the play space, response to the user that call this command.
 
-    response = await axios.get('http://autoinsult.com/index.php?style=3')
-    console.log(response.status);
-    if (response.status == 200) {
-      webpage = new JSDOM(response.data)
-      message.reply(webpage.window.document.querySelector("div.insult").textContent);
-    }
-    else {
-      message.reply("Sorry I had an issue trying to come up with an insult.")
+    try {
+      response = await axios.get("http://autoinsult.com/index.php?style=3");
+      // console.log(response.data.statusCode);
+      if (response.status == 200) {
+        webpage = new JSDOM(response.data);
+        message.reply(
+          webpage.window.document.querySelector("div.insult").textContent
+        );
+      } else {
+        message.reply("Sorry, I couldn't come up with an insult.");
+      }
+    } catch (error) {
+      message.reply("Sorry, I couldn't come up with an insult.");
     }
   }
 });
